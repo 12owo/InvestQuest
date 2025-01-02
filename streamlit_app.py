b07ab6981c7255116899f4100dad00faa5fa3865
12owo <artificialintelligence.owo@gmail.com>
2025-01-02 15:54:15 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,10 @@
 import pandas as pd
 import streamlit as st 
 import yfinance as yf
+
 st.title(' 📈 InvestQuest')
 st.info('Start investing now!') 
+
 # Define the stock ticker and the time period
 ticker = "GOOGL"  # Example: Google stock
 period = "1y"  # Example: 1 year of data
