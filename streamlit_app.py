ea4fdffa06b056ee2127b277b12a47ead32e7a04
12owo <artificialintelligence.owo@gmail.com>
2025-01-02 15:40:33 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,16 +1,2 @@
 st.title(' 📈 InvestQuest')
 st.info('Start investing now!') 
-import streamlit as st
-import pandas as pd 
-import yfinance as yf
-from sklearn.preprocessing import StandardScaler
-# Define the stock ticker and the time period
-ticker = "GOOGL"  # Example: Google stock
-period = "1y"  # Example: 1 year of data
-
-# Fetch the historical market data
-stock_data = yf.Ticker(ticker)
-df = stock_data.history(period=period)
-
-# Display the data
-print(df)
