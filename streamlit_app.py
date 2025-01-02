0206443e209dfa655e30cc768db09a36e0eb5ecc
12owo <artificialintelligence.owo@gmail.com>
2025-01-02 16:36:52 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,7 +36,7 @@ def train_model(df):
     y_pred = model.predict(X_test)
     return model, y_test, y_pred
 
-ticker = st.text_input("Enter Stock Ticker", value="GOOGL",)
+ticker = st.text_input("Enter Stock ", value="GOOGL", "AAPL" , "MSFT" , "AMZN", "NVDA" , "WIT" , "ORCL" , "IBM" , "META" , "TSLA")
 if ticker:
     df = fetch_data(ticker)
     df = preprocess_data(df)
